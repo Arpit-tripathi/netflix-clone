af18173905cfb89c3867c63e4367782daa452bed
Arpit-tripathi <tarpit501@gmail.com>
2023-12-02 13:54:59 +0530

    home

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhfXURABbrEtYU8l3bDzHm7t8q3VrShcw",
+  authDomain: "netflix-clone-f38cb.firebaseapp.com",
+  projectId: "netflix-clone-f38cb",
+  storageBucket: "netflix-clone-f38cb.appspot.com",
+  messagingSenderId: "147422968805",
+  appId: "1:147422968805:web:05cad8c4e1d87114c94d0c",
+};
