62d5496df05b9f82108283c99d46b813050b11e4
Arpit-tripathi <tarpit501@gmail.com>
2023-12-10 23:28:50 +0530

    login page

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAhfXURABbrEtYU8l3bDzHm7t8q3VrShcw",
   authDomain: "netflix-clone-f38cb.firebaseapp.com",
@@ -6,3 +8,4 @@ const firebaseConfig = {
   messagingSenderId: "147422968805",
   appId: "1:147422968805:web:05cad8c4e1d87114c94d0c",
 };
+
